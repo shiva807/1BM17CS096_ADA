80556a14f419de25d2df78f55f4df6d1eec02b65
shiva807 <49604032+shiva807@users.noreply.github.com>
2019-09-04 12:58:57 +0530

    lab 4 program completed

---
+++ lab4a.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
+
+int V;
+list<int>*adj;
+
+void DFSUtil(int v, bool visited[]);
+public:
+Graph(int V);
+void addEdge(int v, int w);
+void connectedcomponents();
+};
+
+Graph::Graph(int V)
+{
+this->V=V;
+adj=new list<int>[V];
+}
+
+void Graph::DFSUtil(int v, bool visited[])
+{
+	visited[v]=true;
+	cout<<v<<" ";
+	
+	list<int>::iterator i;
+	for(i=adj[v].begin(); i!=adj[v].end(); ++i)
+		if(!visited[*i])
+			DFSUtil(*i, visited);
+}
+
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w);
+	adj[w].push_back(v);
+}
+
+void Graph::connectedcomponents()
+{
+	bool *visited=new bool[V];
+	for(int i=0; i<V; i++)
+		visited[i]=false;
+	
+	for(int i=0; i<V; i++)
+	{
+		if(visited[i]==false)
+		{
+			DFSUtil(i, visited);
+			
+			cout<<"\n";
+		}
+	}
+	
+}
+
+int main()
+{
+        Graph g(4);
+	g.addEdge(0,1);
+	g.addEdge(1,2);
+	g.addEdge(0,2);
+	g.addEdge(2,0);
+	g.addEdge(2,3);
+	
+	cout<<"Connected components are: "<<endl;
+	g.connectedcomponents();
+}
